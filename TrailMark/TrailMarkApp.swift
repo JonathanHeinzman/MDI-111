eb66f57d26ef2ecf7720140326b2bab53732c6ae
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-08-03 18:16:22 -0600

    Initial Commit

---
+++ TrailMark/TrailMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMarkApp.swift
+//  TrailMark
+//
+//  Created by Jonathan Heinzman on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
